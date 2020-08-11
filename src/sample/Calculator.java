40ff22090fe33870cb372543e3599203e3f63982
BuildTools <unconfigured@null.spigotmc.org>
2020-08-11 11:44:24 +0100

    Added a choice of operations

--- src/sample/Calculator.java
+++ src/sample/Calculator.java
@@ -1,7 +1,33 @@
 package sample;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Calculator {
     public static void main (String[] args) {
         System.out.println("Welcome to the calculator");
+
+        //Generate 2 random number
+        Random random = new Random();
+        int randomNum1 = random.nextInt(100);
+        int randomNum2 = random.nextInt(100);
+
+        //Choose an operator
+        System.out.println("Input your operator");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        //Perform calculation
+        if (input.equals("+")) {
+            System.out.println(randomNum1 + randomNum2);
+        }
+        else if (input.equals("-")) {
+            System.out.println(randomNum1 - randomNum2);
+        }
+        else if (input.equals("*")) {
+            System.out.println(randomNum1 * randomNum2);
+        }
+        else {
+            System.out.println("Invalid input");
+        }
     }
 }
