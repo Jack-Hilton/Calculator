0b2e3d9c2afab64a06172b28caf59a828dd59f7f
BuildTools <unconfigured@null.spigotmc.org>
2020-08-14 11:52:08 +0100

    Added choice of numbers

--- src/sample/Calculator.java
+++ src/sample/Calculator.java
@@ -6,25 +6,36 @@ public class Calculator {
     public static void main (String[] args) {
         System.out.println("Welcome to the calculator");
 
-        //Generate 2 random number
-        Random random = new Random();
+        //Generate 2 random numbers
+        /*Random random = new Random();
         int randomNum1 = random.nextInt(100);
-        int randomNum2 = random.nextInt(100);
+        int randomNum2 = random.nextInt(100);*/
+
+        //Choose Number 1
+        System.out.println("Input your first number");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = Integer.valueOf(scanner.next());
+
+        //Choose Number 2
+        System.out.println("Input your second number");
+        int num2 = Integer.valueOf(scanner.next());
 
         //Choose an operator
         System.out.println("Input your operator");
-        Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
 
         //Perform calculation
         if (input.equals("+")) {
-            System.out.println(randomNum1 + randomNum2);
+            int result = num1 + num2;
+            System.out.println(num1 + " + " + num2 + " = " + result);
         }
         else if (input.equals("-")) {
-            System.out.println(randomNum1 - randomNum2);
+            int result = num1 - num2;
+            System.out.println(num1 + " - " + num2 + " = " + result);
         }
         else if (input.equals("*")) {
-            System.out.println(randomNum1 * randomNum2);
+            int result = num1 * num2;
+            System.out.println(num1 + " * " + num2 + " = " + result);
         }
         else {
             System.out.println("Invalid input");
