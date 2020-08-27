fc9d5549f9eaee5b8f99252f3196f94e45c157f9
BuildTools <unconfigured@null.spigotmc.org>
2020-08-27 11:36:16 +0100

    Added choice of amount of numbers

--- src/sample/Calculator.java
+++ src/sample/Calculator.java
@@ -4,21 +4,16 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Welcome to the calculator");
-
-        //Choose Number 1
-        System.out.println("Input your first number");
-        Scanner scanner = new Scanner(System.in);
-        int num1 = Integer.valueOf(scanner.next());
-
-        //Choose Number 2
-        System.out.println("Input your second number");
-        int num2 = Integer.valueOf(scanner.next());
+        System.out.println("=========================");
 
         //Choose and Operator
         Calculation calculation = chooseCalculation();
 
+        //Choose Numbers
+        int[] numbers = chooseNumbers();
+
         //Calculate and Output Result
-        int result = calculation.calculate(num1, num2);
+        int result = calculate(calculation, numbers);
         System.out.println("Result = " + result);
     }
 
@@ -38,5 +33,32 @@ public class Calculator {
             default:
                 throw new IllegalArgumentException("You must enter an operator");
         }
+    }
+    private static int[] chooseNumbers() {
+        //Ask for the amount of numbers
+        System.out.println("How many numbers?");
+        Scanner scanner = new Scanner(System.in);
+        int numberAmount = scanner.nextInt();
+
+        //Get the numbers
+        int[] numbers = new int[numberAmount];
+        for (int i = 1; i <= numberAmount; i++) {
+            System.out.println("Input number " + i + ": ");
+            numbers[i-1] = scanner.nextInt();
+        }
+
+        //return the numbers
+        return numbers;
+    }
+
+    private static int calculate (Calculation calculation, int[] numbers) {
+        //Calculates result
+        int result = numbers[0];
+        for (int j = 0; j < numbers.length-1; j++) {
+            result = calculation.calculate(result, numbers[j+1]);
+        }
+
+        //Returns result
+        return result;
     }
 }
